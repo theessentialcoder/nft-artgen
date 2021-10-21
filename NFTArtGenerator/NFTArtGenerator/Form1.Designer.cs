67dbc6099c5d1b4a0700c012c9af49d87b29ba03
The Essential Coder <theessentialcoder@gmail.com>
2021-10-21 14:51:41 -0500

    clean up ui

--- NFTArtGenerator/NFTArtGenerator/Form1.Designer.cs
+++ NFTArtGenerator/NFTArtGenerator/Form1.Designer.cs
@@ -29,13 +29,324 @@ namespace NFTArtGenerator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOrder = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInputFolder = new System.Windows.Forms.TextBox();
+            this.btnAddLayer = new System.Windows.Forms.Button();
+            this.clbLayers = new System.Windows.Forms.CheckedListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnOpenInputFolder = new System.Windows.Forms.Button();
+            this.btnOpenOutputFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 552);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 524);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Art Arrangement";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 524);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "NFT Configuration";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 582);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOpenInputFolder);
+            this.groupBox1.Controls.Add(this.clbLayers);
+            this.groupBox1.Controls.Add(this.btnAddLayer);
+            this.groupBox1.Controls.Add(this.txtInputFolder);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtOrder);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(756, 406);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Order #";
+            // 
+            // txtOrder
+            // 
+            this.txtOrder.Location = new System.Drawing.Point(115, 23);
+            this.txtOrder.Name = "txtOrder";
+            this.txtOrder.Size = new System.Drawing.Size(100, 23);
+            this.txtOrder.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Layer Folder Path";
+            // 
+            // txtInputFolder
+            // 
+            this.txtInputFolder.Location = new System.Drawing.Point(115, 52);
+            this.txtInputFolder.Name = "txtInputFolder";
+            this.txtInputFolder.Size = new System.Drawing.Size(600, 23);
+            this.txtInputFolder.TabIndex = 3;
+            // 
+            // btnAddLayer
+            // 
+            this.btnAddLayer.Location = new System.Drawing.Point(675, 81);
+            this.btnAddLayer.Name = "btnAddLayer";
+            this.btnAddLayer.Size = new System.Drawing.Size(75, 23);
+            this.btnAddLayer.TabIndex = 4;
+            this.btnAddLayer.Text = "Add";
+            this.btnAddLayer.UseVisualStyleBackColor = true;
+            this.btnAddLayer.Click += new System.EventHandler(this.btnAddLayer_Click);
+            // 
+            // clbLayers
+            // 
+            this.clbLayers.FormattingEnabled = true;
+            this.clbLayers.Location = new System.Drawing.Point(6, 110);
+            this.clbLayers.Name = "clbLayers";
+            this.clbLayers.Size = new System.Drawing.Size(744, 292);
+            this.clbLayers.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnOpenOutputFolder);
+            this.groupBox2.Controls.Add(this.txtOutputFolder);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 418);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(756, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Location = new System.Drawing.Point(115, 27);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(600, 23);
+            this.txtOutputFolder.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Output Folder Path";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem1.Text = "Help";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnOpenInputFolder
+            // 
+            this.btnOpenInputFolder.Location = new System.Drawing.Point(721, 52);
+            this.btnOpenInputFolder.Name = "btnOpenInputFolder";
+            this.btnOpenInputFolder.Size = new System.Drawing.Size(29, 23);
+            this.btnOpenInputFolder.TabIndex = 6;
+            this.btnOpenInputFolder.Text = "...";
+            this.btnOpenInputFolder.UseVisualStyleBackColor = true;
+            this.btnOpenInputFolder.Click += new System.EventHandler(this.btnOpenInputFolder_Click);
+            // 
+            // btnOpenOutputFolder
+            // 
+            this.btnOpenOutputFolder.Location = new System.Drawing.Point(721, 27);
+            this.btnOpenOutputFolder.Name = "btnOpenOutputFolder";
+            this.btnOpenOutputFolder.Size = new System.Drawing.Size(29, 23);
+            this.btnOpenOutputFolder.TabIndex = 7;
+            this.btnOpenOutputFolder.Text = "...";
+            this.btnOpenOutputFolder.UseVisualStyleBackColor = true;
+            this.btnOpenOutputFolder.Click += new System.EventHandler(this.btnOpenOutputFolder_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.btnGenerate);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(768, 524);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Generate";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(126, 79);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 604);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NFT Art Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddLayer;
+        private System.Windows.Forms.TextBox txtInputFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOrder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox clbLayers;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnOpenOutputFolder;
+        private System.Windows.Forms.Button btnOpenInputFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button btnGenerate;
     }
 }
 
--- NFTArtGenerator/NFTArtGenerator/Form1.cs
+++ NFTArtGenerator/NFTArtGenerator/Form1.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
 
 namespace NFTArtGenerator
 {
@@ -15,7 +13,120 @@ namespace NFTArtGenerator
         public Form1()
         {
             InitializeComponent();
+            ((ListBox)clbLayers).DisplayMember = "DisplayText";
         }
 
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAddLayer_Click(object sender, EventArgs e)
+        {
+            var displayText = $"Order # {Convert.ToInt32(txtOrder.Text)}, Location: {txtInputFolder.Text}";
+            var item = new LayerInformation(Convert.ToInt32(txtOrder.Text), txtInputFolder.Text, true, displayText);
+            clbLayers.Items.Add(item);
+            txtOrder.Text = string.Empty;
+            txtInputFolder.Text = string.Empty;
+        }
+
+        private void btnOpenInputFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtInputFolder.Text = folderBrowserDialog1.SelectedPath;
+                //Environment.SpecialFolder root = folderBrowserDialog1.RootFolder;
+            }
+        }
+
+        private void btnOpenOutputFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtOutputFolder.Text = folderBrowserDialog1.SelectedPath;
+                //Environment.SpecialFolder root = folderBrowserDialog1.RootFolder;
+            }
+        }
+
+        public static Image GenerateImage(Image backImage, Image frontImage)
+        {
+            int targetHeight = 512; //height and width of the finished image
+            int targetWidth = 512;
+
+            //be sure to use a pixelformat that supports transparency
+            using (var bitmap = new Bitmap(targetWidth, targetHeight, PixelFormat.Format32bppArgb))
+            {
+                using (var canvas = Graphics.FromImage(bitmap))
+                {
+                    //this ensures that the backgroundcolor is transparent
+                    canvas.Clear(Color.Transparent);
+
+                    //this selects the entire backimage and and paints
+                    //it on the new image in the same size, so its not distorted.
+                    canvas.DrawImage(backImage,
+                              new Rectangle(0, 0, backImage.Width, backImage.Height),
+                              new Rectangle(0, 0, backImage.Width, backImage.Height),
+                              GraphicsUnit.Pixel);
+
+                    //this paints the frontimage with a offset at the given coordinates
+                    canvas.DrawImage(frontImage, 0, 0);
+
+                    canvas.Save();
+                }
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    // in this case i needed to return a webimage,
+                    //but you could simply save it to disk, or return a byte array.
+                    //Again, be sure to save in a format that supports transparency,
+                    //such as Png does.
+                    bitmap.Save(memoryStream, ImageFormat.Png);
+                    return new Bitmap(memoryStream);
+                }
+            }
+        }
+
+        public List<LayerInformation> Layers { get; set; } = new();
+        public class LayerInformation
+        {
+            public LayerInformation(int Order, string Location, bool Selected, string DisplayText)
+            {
+                this.Order = Order;
+                this.Location = Location;
+                this.Selected = Selected;
+                this.DisplayText = DisplayText;
+            }
+            public int Order { get; set; }
+            public string Location { get; set; }
+            public bool Selected { get; set; } = true;
+            public string DisplayText { get; set; }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string layer1 = @"C:\\bitmap\Background\\Black#1.png";
+                string layer2 = @"C:\\bitmap\\Layer1\\Middle#40.png";
+
+                Image l1 = Image.FromFile(layer1);
+                Image l2 = Image.FromFile(layer2);
+
+                var res = GenerateImage(l1, l2);
+                res.Save(@"C:\\bitmap\\res1.png");
+                res.Dispose();
+
+                Console.WriteLine("Finished");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
